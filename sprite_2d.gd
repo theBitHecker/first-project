5652d4fb70143b715364428ec587c998c7703f30
Noah Jabir <noah.jabir@gmail.com>
2025-10-21 15:31:22 -0400

    Renamed spear to pencil

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -2,31 +2,37 @@ extends CharacterBody2D
 
 
 
-# Stats (Constants)
+# Stats (Weapon-Dynamic)
 const MOVE_SPEED = 1600
 const SPEED_LIMIT = 400
 const DRAG = 0.9
 const MAX_MANA = 100
 const MANA_RECHARGE = 20
-const SPEAR_SPECIAL_COST = 25
-const SPEAR_SPECIAL_POWER = 2500
-const MAX_SPEAR_SPECIAL_COOLDOWN = 0.5
+const PENCIL_SPECIAL_COST = 25
+const PENCIL_SPECIAL_POWER = 2500
+const MAX_PENCIL_SPECIAL_COOLDOWN = 0.5
+const RULER_SPECIAL_COST = 25
+const RULER_SPECIAL_POWER = 2500
+const MAX_RULER_SPECIAL_COOLDOWN = 0.5
 
-# Stats (Dynamic)
+# Stats (Used-Dynamic)
 var move_speed = MOVE_SPEED
 var speed_limit = SPEED_LIMIT
 var drag = DRAG
 var max_mana = MAX_MANA
 var mana_recharge = MANA_RECHARGE
-var spear_special_cost = SPEAR_SPECIAL_COST
-var spear_special_power = SPEAR_SPECIAL_POWER
-var max_spear_special_cooldown = MAX_SPEAR_SPECIAL_COOLDOWN
-
+var pencil_special_cost = PENCIL_SPECIAL_COST
+var pencil_special_power = PENCIL_SPECIAL_POWER
+var max_pencil_special_cooldown = MAX_PENCIL_SPECIAL_COOLDOWN
+var ruler_special_cost = RULER_SPECIAL_COST
+var ruler_special_power = RULER_SPECIAL_POWER
+var max_ruler_special_cooldown = MAX_RULER_SPECIAL_COOLDOWN
 
 var mana = max_mana
-var spear_special_cooldown = max_spear_special_cooldown
+var pencil_special_cooldown = max_pencil_special_cooldown
+var ruler_special_cooldown = max_ruler_special_cooldown
 
-var dashing = false
+var penciling = false
 var dash_direction = Vector2(1,0)
 @onready var mana_bar = $ManaBar
 #@onready var camera = $Camera2D
@@ -43,35 +49,35 @@ func _physics_process(delta: float) -> void:
 	# Abilitiesd
 
 	if Input.is_action_just_pressed("Dash"):
-		if spear_special_cooldown == 0:
-			if mana >= spear_special_cost:
-				spear_special_cooldown = max_spear_special_cooldown
-				mana -= spear_special_cost
+		if pencil_special_cooldown == 0:
+			if mana >= pencil_special_cost:
+				pencil_special_cooldown = max_pencil_special_cooldown
+				mana -= pencil_special_cost
 				if velocity.length() > 0.01:
 					dash_direction = velocity.normalized()
 				mana_recharge = 0
-				velocity -= 0.1*spear_special_power*dash_direction
+				velocity -= 0.1*pencil_special_power*dash_direction
 				await get_tree().create_timer(0.1).timeout
-				velocity += 1.1*spear_special_power*dash_direction
+				velocity += 1.1*pencil_special_power*dash_direction
 				mana_recharge = MANA_RECHARGE
-				dashing = true
+				penciling = true
 	# Speed Limit
 	if velocity.x > speed_limit:
-		if not dashing:
+		if not penciling:
 			velocity.x = speed_limit
 	if velocity.x < -speed_limit:
-		if not dashing:
+		if not penciling:
 			velocity.x = -speed_limit
 	if velocity.y > speed_limit:
-		if not dashing:
+		if not penciling:
 			velocity.y = speed_limit
 	if velocity.y < -speed_limit:
-		if not dashing:
+		if not penciling:
 			velocity.y = -speed_limit
 			
 	# Dash Handling 
 	if velocity.length() < speed_limit:
-		dashing = false
+		penciling = false
 		
 	# Slowing Down
 	velocity *= drag
@@ -83,13 +89,13 @@ func _physics_process(delta: float) -> void:
 		mana += mana_recharge * delta
 	mana_bar.value = (mana/max_mana)*100
 	
-	if spear_special_cooldown > 0:
-		spear_special_cooldown -= delta
+	if pencil_special_cooldown > 0:
+		pencil_special_cooldown -= delta
 	else:
-		spear_special_cooldown = 0
+		pencil_special_cooldown = 0
 		
 	# Collision
-	if dashing:
+	if penciling:
 		var collision = move_and_collide(velocity*delta)
 		if not collision == null:
 			velocity = velocity.bounce(collision.get_normal())
